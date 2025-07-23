10aab217a43cf361333a62428f83f7b6e332fc6f
Zain Gillani <abdullahgillaniv204@gmail.com>
2025-07-23 14:23:16 +0500

    .

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,18 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
     plugins: [
         laravel({
-            input: "resources/js/app.jsx",
+            input: ['resources/js/app.jsx'],
             refresh: true,
         }),
         react(),
     ],
+    build: {
+        manifest: true,
+        outDir: 'public/build',
+        rollupOptions: {
+            input: {
+                app: 'resources/js/app.jsx',
+            },
+        },
+    },
 });
